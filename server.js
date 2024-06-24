c509ea211ce43e63114ccc410d233636d00cf8f5
rezaeination <toron9119@gmail.com>
2024-06-25 03:18:44 +0400

    Update server.js

--- server.js
+++ server.js
@@ -261,6 +261,7 @@ async function scrapeLocal(url, parentFolderId) {
       resources: new CustomResourceLoader(), // Use custom resource loader
       virtualConsole: new JSDOM.VirtualConsole().sendTo(console),
       pretendToBeVisual: true, // Pretend to be visual to reduce CSS parsing issues
+      parsingMode: "html" // Set parsing mode to HTML only
     });
 
     // Set a memory usage limit (in bytes)
@@ -302,6 +303,42 @@ async function scrapeLocal(url, parentFolderId) {
   } finally {
     // Ensure resources are cleaned up
     response = null; // Nullify axios response object
+  }
+}
+
+async function getUrlsFromSitemap(sitemapUrl) {
+
+  // Fetch the sitemap
+  let urls = [];
+  try {
+    let array = await sitemap.fetch(sitemapUrl);
+    array = array.sites
+    console.log(array);
+
+    // Function to check if URL contains certain words
+    const containsForbiddenWords = url => {
+      const forbiddenWords = ['mp4', 'mp3', 'png', 'jpg', 'jpeg'];
+      for (const word of forbiddenWords) {
+        if (url.includes(word)) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    for (const url of array) {
+      // Check if the URL contains forbidden words
+      if (!containsForbiddenWords(url)) {
+        urls.push(url);
+      }
+    }
+
+    // Return the 10 most recent URLs if there are more than 10
+    console.log(urls.slice(0, 10));
+    return urls.slice(0, 10);
+  } catch (error) {
+    console.error('Error fetching sitemap:', error);
+    return [];
   }
 }
 
